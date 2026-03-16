3e58a1144552251435e2c082b9b74c3a2aef21f9
borno-22 <ornob.2202@gmail.com>
2026-03-16 09:54:00 +0600

    total calculated amount

--- Lab Performance_MID/JS/Validation.js
+++ Lab Performance_MID/JS/Validation.js
@@ -3,3 +3,19 @@ const days = 30;
 
 let quantityInput = document.getElementById('quantity');
 let totalPriceInput = document.getElementById('totalPrice');
+
+function calculateTotal() {
+    let quantity = quantityInput.value;
+    if (quantityInput.value == "") {
+        quantity = 0;
+    }
+
+    if (quantity < 0) {
+        quantity = 0;
+        quantityInput.value = 0;
+    }
+
+    let total = unitPrice * quantity * days;
+    totalPriceInput.value = total;
+
+}
