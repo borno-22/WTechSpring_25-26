77d451b9d57d24e5e602b28feec5e323c7b15497
borno-22 <ornob.2202@gmail.com>
2026-03-16 10:02:24 +0600

    alert added

--- Lab Performance_MID/JS/Validation.js
+++ Lab Performance_MID/JS/Validation.js
@@ -18,4 +18,9 @@ function calculateTotal() {
     let total = unitPrice * quantity * days;
     totalPriceInput.value = total;
 
+    if (total > 1000) {
+      alert("Congratulations! You are eligible for a gift coupon!");
+    }
 }
+
+quantityInput.addEventListener('input', calculateTotal);
